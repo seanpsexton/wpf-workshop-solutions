f32111299911bf89607273aae694e274c7e2fd1c
Sean Sexton <sean.sexton@mts.com>
2023-09-27 10:00:29 -0500

    Solution for exercise 4

---
+++ Exercises/Exercise-4-BindingToList-Solution/Exercise_4_BindingToList_Solution/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4_BindingToList_Solution
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private List<Person> people;
+        private Person selectedPerson;
+
+        public MainViewModel()
+        {
+            people = new List<Person>()
+            {
+                new Person("Charles", "Chaplin", 1889, 1977),
+                new Person("Mary", "Pickford", 1892, 1979),
+                new Person("Spencer", "Tracy", 1900, 1967),
+                new Person("Katharine", "Hepburn", 1907, 2003)
+            };
+        }
+
+        public List<Person> People
+        {
+            get => people; 
+            set => SetValue(ref people, value);
+        }
+
+        public Person SelectedPerson
+        {
+            get => selectedPerson;
+            set => SetValue(ref selectedPerson, value);
+        }
+    }
+}
---
+++ Exercises/Exercise-4-BindingToList-Solution/Exercise_4_BindingToList_Solution/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4_BindingToList_Solution
+{
+    public class Person : ViewModelBase
+    {
+        private string firstName;
+        private string lastName;
+        private int birth;
+        private int death;
+
+        public Person(string firstName, string lastName, int birth, int death)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birth = birth;
+            Death = death;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => SetValue(ref firstName, value);
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => SetValue(ref lastName, value);
+        }
+
+        public int Birth
+        {
+            get => birth;
+            set => SetValue(ref birth, value);
+        }
+
+        public int Death
+        {
+            get => death;
+            set => SetValue(ref death, value);
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {firstName}";
+        }
+    }
+}
